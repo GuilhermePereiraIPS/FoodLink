c587ff9ad42f44c1f235085f8311c88eb4342597
André <98454431+pererere@users.noreply.github.com>
2025-01-14 18:52:04 +0000

    angular components and services added

--- foodlink.client/src/app/app.module.ts
+++ foodlink.client/src/app/app.module.ts
@@ -5,11 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    NavMenuComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
