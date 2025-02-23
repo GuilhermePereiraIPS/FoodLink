9b94ee404d4db67dbe0e5d9b2c2b33d5e5e40199
Leonardo Zacarias <202101132@estudantes.ips.pt>
2025-02-23 10:47:09 +0000

    Create Comment
    Initial structure of comments

---
+++ FoodLink.Server/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodLink.Server.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int IdComment { get; set; }
+
+        [Required]
+        public string? CommentText { get; set; }
+        public int RecipeId { get; set; }
+        public int UserId { get; set; }
+    }
+}
