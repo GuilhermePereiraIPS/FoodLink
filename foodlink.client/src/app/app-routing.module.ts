5f9bc3262da9270ee34a45e7abc44102f3483021
pere <202001403@estudantes.ips.pt>
2025-03-02 15:28:03 +0000

    login redirect

--- foodlink.client/src/app/app-routing.module.ts
+++ foodlink.client/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component'
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component'
 import { RecipeCreateComponent } from './recipe-create/recipe-create.component'
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { AuthGuard } from './api-authorization/authorize.guard';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: 'signin', pathMatch: 'full' },
-  { path: 'recipes/create', component: RecipeCreateComponent },
-  { path: 'recipes', component: RecipeListComponent },
-  { path: 'recipes/:id', component: RecipeDetailsComponent },
-  { path: 'recipes/edit/:id', component: RecipeEditComponent },
+  { path: 'signin', redirectTo: 'signin'},
+  { path: 'recipes/create', component: RecipeCreateComponent, canActivate: [AuthGuard] },
+  { path: 'recipes', component: RecipeListComponent, canActivate: [AuthGuard] },
+  { path: 'recipes/:id', component: RecipeDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'recipes/edit/:id', component: RecipeEditComponent, canActivate: [AuthGuard] },
 
   //{ path: '/recipes', component: RecipeListComponent}
 ];
--- foodlink.client/src/app/app.component.ts
+++ foodlink.client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthorizeService } from './api-authorization/authorize.service';
 
 
 @Component({
@@ -21,7 +22,16 @@ export class AppComponent {
 
   //title = 'foodlink.client';
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private authService: AuthorizeService) {
+
+    if (this.router.url === '/') {
+      if (this.authService.isSignedIn()) {
+        this.router.navigate(['/recipes']); 
+      } else {
+        this.router.navigate(['/signin']); 
+      }
+    }
+
     this.router.events.subscribe(() => {
       // Esconder o footer quando a página for /signin
       this.showFooter = !['/signin', '/new'].includes(this.router.url);
