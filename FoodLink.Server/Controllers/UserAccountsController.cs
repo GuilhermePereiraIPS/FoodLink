5ca5b9ac69225315aa9c6d2305b2a5c7ea3e4bb7
pere <202001403@estudantes.ips.pt>
2025-03-08 18:11:38 +0000

    Update user info

--- FoodLink.Server/Controllers/UserAccountsController.cs
+++ FoodLink.Server/Controllers/UserAccountsController.cs
@@ -85,6 +85,108 @@ namespace FoodLink.Server.Controllers
             });
         }
 
+
+        /// <summary>
+        /// Retorna o utilizador autenticado
+        /// </summary>
+        /// <returns>
+        /// Retorna um <see cref="UserProfileModel"/> contendo o ID, nome e email do utilizador.
+        /// Se o utilizador não estiver autenticado, retorna uma resposta Unauthorized (401).
+        /// </returns>
+        [HttpGet("api/currentUserId")]
+        [Authorize]
+        public async Task<IActionResult> GetCurrentUserID()
+        {
+            ClaimsPrincipal currentUser = this.User;
+            var currentUserIDClaim = currentUser.FindFirst("UserId");
+
+            if (currentUserIDClaim == null) return Unauthorized(new { message = "User ID claim not found." });
+
+            var currentUserID = currentUserIDClaim.Value;
+            return Ok(new { userID = currentUserID });
+        }
+
+        /// <summary>
+        /// Atualiza informações do utilizador dependendo do que é enviado
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPut("api/updateCurrentUser")]
+        [Authorize]
+        public async Task<IActionResult> UpdateCurrentUser([FromBody] UserUpdateModel model)
+        {
+            ApplicationUser user = await GetCurrentUser();
+            if (user == null) return NotFound(new { message = "User not found." });
+            
+            // Username
+            if (!string.IsNullOrEmpty(model.Username) && model.Username != user.UserName)
+            {
+                var emailExists = await _userManager.FindByEmailAsync(model.Email);
+                user.UserName = model.Username;
+            }
+
+            // Email
+            if (!string.IsNullOrEmpty(model.Email) && model.Email != user.Email)
+            {
+                var emailExists = await _userManager.FindByEmailAsync(model.Email);
+                if (emailExists != null) return BadRequest(new { message = "Email already in use." });
+                
+                user.Email = model.Email;
+            }
+
+            // Password
+            if (!string.IsNullOrEmpty(model.Password))
+            {
+                var passwordValid = await _userManager.CheckPasswordAsync(user, model.CurrentPassword);
+                if (!passwordValid)
+                {
+                    return BadRequest(new { message = "Current password is incorrect." });
+                }
+
+                var passwordChangeResult = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.Password);
+                if (!passwordChangeResult.Succeeded)
+                {
+                    return BadRequest(passwordChangeResult.Errors);
+                }
+            }
+
+            var updateResult = await _userManager.UpdateAsync(user);
+
+            if (updateResult.Succeeded)
+            {
+                return Ok(new { message = "User information updated successfully." });
+            }
+
+            return BadRequest(updateResult.Errors);
+        }
+
+        private async Task<bool> IsPasswordValid(ApplicationUser user, string password)
+        {
+            return await _userManager.CheckPasswordAsync(user, password);
+        }
+
+        private async Task<ApplicationUser> GetCurrentUser()
+        {
+            ClaimsPrincipal currentUser = this.User;
+            var currentUserIDClaim = currentUser.FindFirst("UserId");
+
+            if (currentUserIDClaim == null) return null;
+
+            var currentUserID = currentUserIDClaim.Value;
+            var user = await _userManager.FindByIdAsync(currentUserID);
+
+            return user;
+        }
+    }
+
+
+
+    public class UserUpdateModel
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? CurrentPassword { get; set; } // Needed to check if the current password is correct when updating
     }
 
     public class UserLoginModel
