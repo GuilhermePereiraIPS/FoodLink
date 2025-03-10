94017f621b70da44763202a126f8da2ae4dac379
Leonardo Zacarias <202101132@estudantes.ips.pt>
2025-03-10 18:04:05 +0000

    Class RecipeBook

---
+++ FoodLink.Server/Models/RecipeBook.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodLink.Server.Models
+{
+    public class RecipeBook
+    {
+        [Key]
+        public int IdRecipeBook { get; set; }
+
+        [Required]
+        public string? RecipeBookTitle { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+    }
+}
