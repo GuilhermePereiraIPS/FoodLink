9fab753dfa262a98ffab44dde84c95e33acfdbc5
André <202001403@estudantes.ips.pt>
2025-03-27 11:58:55 +0000

    comment delete permissions

--- FoodLink.Server/Migrations/20250327112134_iniial.cs
+++ FoodLink.Server/Migrations/20250327112134_iniial.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace FoodLink.Server.Migrations
 {
     /// <inheritdoc />
-    public partial class initial : Migration
+    public partial class iniial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
