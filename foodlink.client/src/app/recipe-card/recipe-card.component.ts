a6321c1cbe2775347296e20fcc21d8b424ba8fe9
André <202001403@estudantes.ips.pt>
2025-03-31 11:34:35 +0100

    recipe book -> recipe remove html

--- foodlink.client/src/app/recipe-card/recipe-card.component.ts
+++ foodlink.client/src/app/recipe-card/recipe-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AccountsService, User } from '../services/accounts.service';
+import { RecipeBooksService } from '../services/recipe-books.service';
+import { RecipeBookDetailsComponent } from '../recipe-book-details/recipe-book-details.component'
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-card',
@@ -13,13 +16,15 @@ export class RecipeCardComponent implements OnInit {
   @Input() date: string | null = null;
   @Input() imageUrl: string = '';
   @Input() description: string = '';
+  @Input() recipeBookId!: number;
+  @Input() onClickRemove!: (recipeBookId: number, recipeId: number) => void;
 
   username!: string
 
-  constructor(private accountsService: AccountsService) { }
+  constructor(private accountsService: AccountsService, private recipeBooksService: RecipeBooksService) { }
 
   ngOnInit(): void {
-    if (this.userId) {
+    if (this.userId) { 
       this.getUser(this.userId); 
     }
   }
@@ -35,5 +40,11 @@ export class RecipeCardComponent implements OnInit {
         this.username = 'Unknown User'; 
       }
     );
+  }
+
+  onRemoveClick() {
+    if (this.onClickRemove) {
+      this.onClickRemove(this.recipeBookId, this.id);  
+    }
   }
 }
